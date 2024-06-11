fa8779fffdbac1a9bc97fffeef525d272b3edfc7
unknown <carol.karol@live.com>
2024-06-11 13:12:48 -0300

    Testes Insomnia

---
+++ src/main/java/com/blogpessoal/model/Postagem.java
@@ -0,0 +1,88 @@
+package com.blogpessoal.model;
+
+import java.time.LocalDateTime;
+
+import org.hibernate.annotations.UpdateTimestamp;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+
+@Entity// classe vai se tornar uma entidade no banco de dados(tabelas)
+@Table(name="tb_postagens")//nomeando a tabela no banco de dados de tb_postagens
+public class Postagem {
+	
+	
+	@Id // tornando o campo uma chave primária no banco de dados
+	@GeneratedValue(strategy = GenerationType.IDENTITY)// tornando a chave primária auto-increment
+	private Long id;
+	
+	
+	@NotBlank(message ="O atributo título é obrigatório!")// validation - validar nosso atributo NN e também não vazio
+	@Size(min = 5, max = 100, message="O atributo TITULO deve ter no mínimo 5 caracteres e no máximo 100 caracteres.")
+    private String titulo;
+	
+	
+	@NotBlank (message ="O atributo TEXTO é obrigatório!")
+	@Size(min = 10, max = 1000,  message = "O atributo TEXTO deve ter no mínimo 10 caracteres e no máximo 1000 caracteres")
+	private String texto;
+	
+	@UpdateTimestamp
+	private LocalDateTime data;
+	
+	
+	@ManyToOne
+	@JsonIgnoreProperties("Postagem")
+	private Tema tema;
+	
+	@ManyToOne
+	@JsonIgnoreProperties("postagem")
+	private Usuario usuario;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getTitulo() {
+		return titulo;
+	}
+
+	public void setTitulo(String titulo) {
+		this.titulo = titulo;
+	}
+
+	public String getTexto() {
+		return texto;
+	}
+
+	public void setTexto(String texto) {
+		this.texto = texto;
+	}
+
+	public LocalDateTime getData() {
+		return data;
+	}
+
+	public void setData(LocalDateTime data) {
+		this.data = data;
+	}
+
+	public Tema getTema() {
+		return tema;
+	}
+
+	public void setTema(Tema tema) {
+		this.tema = tema;
+	}
+}
